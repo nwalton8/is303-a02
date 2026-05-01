5f94182d10710c7fd5c831502e654a7000283062
Noah Walton <nwalton8@byu.edu>
2026-05-01 02:37:01 +0000

    start

---
+++ quest_recommender.py
@@ -0,0 +1,42 @@
+'''
+Noah Walton
+IS 303 - A02
+
+Quest Recommender
+This program recommends quests based on player level and player class.
+
+Inputs:
+- Player name (string)
+- Player level (integer)
+- Player class (string)
+
+Processes:
+- Validate level (must be positive)
+- Validate class (must be one of the supported classes)
+- Determine quest difficulty based on level
+- Recommend quests based on class and difficulty
+
+Outputs:
+- Print recommended quests
+- Print error message if any input is invalid
+'''
+
+print("Welcome to the Quest Recommender!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#'''
